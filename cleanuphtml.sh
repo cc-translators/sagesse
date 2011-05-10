e3d67d31d53ad814df44dcfb4aed51bd42d19fed
Raphael Pinson <raphink@gmail.com>
2011-05-10 22:49:32 +0200

    Make specific \secstyle and \seclbl for HTML

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -22,7 +22,7 @@ sed -i 's@\(id="x1-[0-9]\+r[0-9]\+"\)>@\1 class="pagebreak">@' $HTML
 
 
 # Fix small caps
-for class in "fxlrc-t1-" "fxlrc-t1-x-x-120"; do
+for class in "fxlrc-t1-" "fxlrc-t1-x-x-120" "fxlbc-t1-x-x-248"; do
    while read c r; do
       sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
    done <<<"é É
@@ -30,6 +30,7 @@ for class in "fxlrc-t1-" "fxlrc-t1-x-x-120"; do
 ê Ê
 ﬁ FI
 ï Ï
+â Â
 à À"
 done
 
