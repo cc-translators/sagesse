722e9e729c088de6b12905cd25c2bb605f593a57
Raphaël Pinson <raphink@gmail.com>
2011-05-10 16:44:46 +0200

    Fix small caps

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -17,7 +17,7 @@ sed -i 's@\(id="x1-[0-9]\+r[0-9]\+"\)>@\1 class="pagebreak">@' $HTML
 # Fix small caps
 for class in "fxlrc-t1-" "fxlrc-t1-x-x-120"; do
    while read c r; do
-      sed -i "s@\"$class\">$c\([^<]*\)<@\"small-caps\">$r\1<@" $HTML
+      sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
    done <<<"é É
 è È
 à À"
