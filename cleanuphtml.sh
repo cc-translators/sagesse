d62205b301102fdcc21ceed62450c40b3b11a6ad
Raphaël Pinson <raphink@gmail.com>
2011-05-10 16:55:54 +0200

    Various fixes in HTML

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -6,8 +6,16 @@ CSS="$(basename $HTML .html).css"
 # Replace long lines with <hr />
 sed -i 's@___\+@<hr />@' $HTML
 
+# Center title page
+echo ".titlepage {text-align:center;}" >> $CSS
 
 # Force page breaks in intro
+echo ".pagebreak {page-break-before: always}" >> $CSS
+## Before thanks
+sed -i 's@\(<!--l. 2--><p class="indent\)" >$@\1 pagebreak" >@' $HTML
+
+# Fix missing italic
+echo ".fxlri-t-1 {font-style:italic;}" >> $CSS
 
 ## Before each day
 echo ".pagebreak {page-break-before: always}" >> $CSS
@@ -20,6 +28,8 @@ for class in "fxlrc-t1-" "fxlrc-t1-x-x-120"; do
       sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
    done <<<"é É
 è È
+ê Ê
+ﬁ FI
 à À"
 done
 
