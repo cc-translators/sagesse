2dc03a6f28f609c1872d757141a4c63462aa3d48
Raphaël Pinson <raphink@gmail.com>
2012-08-05 19:02:14 +0200

    Move TeX4HT hacks to .4ht files

--- cleanuphtml.sh
+++ cleanuphtml.sh
@@ -24,40 +24,6 @@ echo ".fxlri-t-1 {font-style:italic;}" >> $CSS
 sed -i 's@\(id="x1-[0-9]\+r[0-9]\+"\)>@\1 class="pagebreak">@' $HTML
 
 
-# Fix small caps
-
-## Accents
-for class in "fxlrc-t1-" "fxlrc-t1-x-x-120" "fxlbc-t1-x-x-248" "fxlbc-t1-x-x-144"; do
-   while read c r; do
-      sed -i "s@\(\"$class\">\)$c\([^<]*\)<@\1<span class=\"small-caps\">$r\2</span><@" $HTML
-   done <<<"é É
-è È
-ê Ê
-ï Ï
-î Î
-ﬁ FI
-ﬀ  FF
-ﬂ FL
-ﬃ   FFI
-â Â
-à À
-ù Ù
-û Û
-ô Ô"
-done
-
-## Ligatures
-while read c r; do
-   sed -i "s@\"small-caps\">$c<@\"small-caps\">$r<@" $HTML
-done <<<"ﬁ FI
-ﬀ  FF
-ﬂ FL
-ﬃ   FFI"
-
-
-# Improve lettrines
-echo ".fxlr-t-1x-x-318 {color:gray;float:left;font-size:4em;margin-right:3px;display:inline;line-height:0.5em;}" >> $CSS
-
 
 # Remove months added (for an unknown reason)
 #   -- ugly!
